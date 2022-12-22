24e7a701df13a876f29e6ff9299bd64a2e213cd6
Trent Henderson <trent.henderson1@outlook.com>
2022-12-22 20:52:59 +1100

    list attempt

--- src/fluctanal_prop_r1.cpp
+++ src/fluctanal_prop_r1.cpp
@@ -1,5 +1,55 @@
 #include <Rcpp.h>
+#include <algorithm>
 using namespace Rcpp;
+
+// Define a sequence generation helper function
+Rcpp::NumericVector get_sequence(double start, double end, int length) {
+  Rcpp::NumericVector out(length);
+  double step = (end - start) / (length - 1);
+  for (int i = 0; i < length; i++) {
+    out[i] = start + i * step;
+  }
+  return out;
+}
+
+// Define a rounding helper function
+Rcpp::IntegerVector round_vector(Rcpp::NumericVector x) {
+  int n = x.size();
+  Rcpp::IntegerVector out(n);
+  for (int i = 0; i < n; i++) {
+    out[i] = std::round(x[i]);
+  }
+  return out;
+}
+
+// Define a unique finder helper function
+Rcpp::IntegerVector unique_elements(Rcpp::IntegerVector x) {
+  std::sort(x.begin(), x.end());  // sort the input vector
+  auto it = std::unique(x.begin(), x.end());  // remove duplicates
+  x.erase(it, x.end());  // erase the removed elements
+  return x;  // return the modified vector
+}
+
+// Define C++ version of `split`
+Rcpp::List split(Rcpp::NumericVector x, Rcpp::NumericVector f) {
+  int n = x.size();
+  int m = f.size();
+  Rcpp::List out(m + 1);  // create a list with m + 1 elements
+  int start = 0;
+  for (int i = 0; i < m; i++) {
+    auto it = std::find(x.begin() + start, x.end(), f[i]);  // find the next split point
+    if (it == x.end()) {  // if the split point is not found
+      out[i] = x[Rcpp::Range(start, n - 1)];  // copy the remaining values to the output list
+      break;  // exit the loop
+    } else {  // if the split point is found
+      int end = std::distance(x.begin(), it);  // index of the split point
+      out[i] = x[Rcpp::Range(start, end - 1)];  // copy the values up to the split point
+      start = end;  // update the start index
+    }
+  }
+  out[m] = x[Rcpp::Range(start, n - 1)];  // copy the remaining values to the last element of the output list
+  return out;
+}
 
 //' Calculate fluctuation analysis from software package \code{hctsa}
 //'
@@ -25,7 +75,7 @@ double fluctanal_prop_r1(NumericVector x) {
   NumericVector x_NA0 = ifelse(!is_na(x), x, 0);
 
   NumericVector y = cumsum(x_NA0);
-  NumericVector taur = unique(round(exp(seq(log(5), log(floor(N / 2)), length = tauStep))));
+  IntegerVector taur = unique_elements(round_vector(exp(get_sequence(log(5), log(floor(N / 2)), tauStep))));
   int ntau = taur.size();
   if (ntau < 8) {
     stop("This time series is too short to analyse using this fluctuation analysis");
@@ -35,7 +85,7 @@ double fluctanal_prop_r1(NumericVector x) {
 
   for (int i = 0; i < ntau; i++) {
     int tau = taur[i];
-    NumericVector y_buff = split(y, ceiling(seq_along(y) / tau));
+    List y_buff = List::create(split(y, ceiling(seq_along(y) / tau)));
 
     if (y_buff.size() > floor(y.size() / tau)) {
       y_buff = y_buff[Range(0, y_buff.size() - 2)];
